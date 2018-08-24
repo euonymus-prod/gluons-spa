76322b2610e08be9283650079ed44a741548b49e
euonymus <euonymus0220@gmail.com>
2018-08-24 16:46:30 +0900

    api locale bug

--- src/statics.js
+++ src/statics.js
@@ -7,8 +7,17 @@ if (process.env.NODE_ENV === 'development') {
     api_key        = 'euonymus';
 }
 
-// NOTE: This expects index.js sets locale in localStorage
 let locale = JSON.parse(localStorage.getItem('locale'));
+if (!locale) {
+    // NOTE: subdomain check must be done domesticaly here. statics.js cannot rely on othere sources.
+    const domainString = document.domain;
+    const domainFirstPart = domainString.split('.')[0]
+    if (domainFirstPart === 'ja') {
+	locale = 'ja'
+	// This is required to manually differ the behavior depends on locale.
+	localStorage.setItem('locale', JSON.stringify(locale));
+    }
+}
 if (locale) {
     api_host = locale + '.' + api_host
 }

d96df1c975f7ee648666d8eba3481072e4baaba4
euonymus <euonymus0220@gmail.com>
2018-09-17 21:57:45 +0900

    bugfix

--- src/reducers/reducer_current_quarks.js
+++ src/reducers/reducer_current_quarks.js
@@ -1,4 +1,4 @@
-import { FETCH_QUARKS, FETCH_QUARKS_FAILURE, SEARCH_QUARKS, INIT_QUARKS } from '../types/quark';
+import { FETCH_QUARKS, FETCH_QUARKS_FAILURE, SEARCH_QUARKS, SEARCH_QUARKS_FAILURE, INIT_QUARKS } from '../types/quark';
 import { CHANGE_PRIVACY } from '../types/privacy';
 
 const initState = {results:[], pagination:{current_page:0, has_next:true, has_prev:false}};
@@ -14,7 +14,9 @@ export default (state = initState, action) => {
 	return {results:[...state.results, ...action.payload.response.results], pagination:action.payload.response.pagination};
 
     case FETCH_QUARKS_FAILURE:
-	return [{'error': 'failed to fetch'}]
+    case SEARCH_QUARKS_FAILURE:
+	//return [{'error': 'failed to fetch'}]
+	return {results:[], pagination:{current_page:0, has_next:false, has_prev:false}, error: 'failed to fetch'};
 
     case INIT_QUARKS:
     case CHANGE_PRIVACY:

6a1dd120dc97e90e2a0bc9dbd30f6672c6f1f66c
euonymus <euonymus0220@gmail.com>
2018-11-21 10:42:57 +0900

    security fix

--- src/components/main_quark.js
+++ src/components/main_quark.js
@@ -7,41 +7,41 @@ import FollowButton from './follow_button'
 
 class MainQuark extends Component {
 
-    render () {
-        const { quark } = this.props;
-        if (!quark) {
+  render () {
+    const { quark } = this.props;
+    if (!quark) {
 	    return <div>Loading...</div>;
-        }
-
-	return (
-           <div className="col-md-3 card subject-main">
-               <div className="subject-image">
-                   <img src={quark.image_path} className="card-img-top" alt={quark.name} />
-               </div>
-
-               <div className="card-block">
-                   <h1 className="card-title">{quark.name}
-            {(() => { if (quark.url) return (
-                      <sub>
-                         <a href={quark.url} target="_blank" className="glyphicon glyphicon-globe"> </a>
-                      </sub>
-	    );})()}
-                   </h1>
-                   <p>{quark.period_str}</p>
-                   <p>{quark.description}</p>
-
-            {(() => { if (quark.affiliate) return (
-		    <p><a href={quark.affiliate} target="_blank">
-                       <FormattedMessage id="button_buy" defaultMessage={`Buy Now`} />
-   		    </a></p>
-	    );})()}
-
-                    <p><Link to={`/relations/add/${quark.id}`} className="btn btn-primary">Add relation</Link></p>
-                    <FollowButton />
-               </div>
-           </div>
-	);
     }
+
+	  return (
+      <div className="col-md-3 card subject-main">
+        <div className="subject-image">
+          <img src={quark.image_path} className="card-img-top" alt={quark.name} />
+        </div>
+
+        <div className="card-block">
+          <h1 className="card-title">{quark.name}
+            {(() => { if (quark.url) return (
+               <sub>
+                 <a href={quark.url} target="_blank" rel="noopener noreferrer" className="glyphicon glyphicon-globe"> </a>
+               </sub>
+	          );})()}
+          </h1>
+          <p>{quark.period_str}</p>
+          <p>{quark.description}</p>
+
+          {(() => { if (quark.affiliate) return (
+		         <p><a href={quark.affiliate} target="_blank" rel="noopener noreferrer" >
+               <FormattedMessage id="button_buy" defaultMessage={`Buy Now`} />
+   		       </a></p>
+	        );})()}
+
+          <p><Link to={`/relations/add/${quark.id}`} className="btn btn-primary">Add relation</Link></p>
+          <FollowButton />
+        </div>
+      </div>
+	  );
+  }
 }
 
 export default MainQuark;

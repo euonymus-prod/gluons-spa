9ae61bd124c374f9e611e257860f7f32535bf56c
euonymus <euonymus0220@gmail.com>
2019-06-03 19:28:40 +0900

    chore

--- functions/index.js
+++ functions/index.js
@@ -10,13 +10,33 @@ const fb_user_log = user_log_id => fb_user_logs().doc(user_log_id)
 
 // NOTE: HTTP request trigger sample
 exports.helloWorld = functions.https.onRequest((request, response) => {
+
+  const fields = ['timestamp', 'uuid', 'locale', 'is_session_start', 'quark_id', 'quark_name']
+
   
   fb_user_logs().get().then(snapshot => {
     const res = []
     snapshot.forEach(user_log => {
+      generateCsvRecord(user_log.data())
       res.push(user_log.data())
     })
-    console.log(res)
+    // console.log(res)
     response.send(res)
   })
 })
+
+function generateCsvRecord(data) {
+  
+  console.log(data.timestamp._seconds)
+  console.log(data.uuid)
+  console.log(data.locale)
+  console.log(data.is_session_start)
+  console.log(data.quark_id)
+  console.log(data.quark_name)
+}
+
+// function convertCsvStr(data) {
+//   const escMsg = msg.replaceAll("\"", "\\\\\"")
+//   console.log(escMsg)
+//   // escMsg.replaceAll("'", "\\\\'")
+// }

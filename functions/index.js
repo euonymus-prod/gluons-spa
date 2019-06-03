443389f05e162caf7019f830ff4e1dc75e0b631f
euonymus <euonymus0220@gmail.com>
2019-06-03 18:57:05 +0900

    try

--- functions/index.js
+++ functions/index.js
@@ -2,24 +2,21 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
 const fb_user_logs = () => admin.firestore().collection('user_logs')
 const fb_user_log = user_log_id => fb_user_logs().doc(user_log_id)
 
 // NOTE: HTTP request trigger sample
 exports.helloWorld = functions.https.onRequest((request, response) => {
-
+  
   fb_user_logs().get().then(snapshot => {
+    const res = []
     snapshot.forEach(user_log => {
-      console.log(user_log.data())
+      res.push(user_log.data())
     })
+    console.log(res)
+    response.send(res)
   })
-
-  response.send("Hello from Firebase!")
 })
